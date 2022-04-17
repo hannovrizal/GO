b161a699298f8ccd72b7680de9297b3bd0c1f57d
hannovrizal <hannovrizal@gmail.com>
2022-04-18 01:50:33 +0700

    Nambahin pengisian data awal

--- Main.go
+++ Main.go
@@ -1,32 +1,38 @@
 package main
+
 import "fmt"
 
-func main(){
+func main() {
 
 	var userNama string
-	var dataArray = [3]string{"Lala","Lilil","Lulut"}
+	/*
+		var dataArray = [3]string{"Lala","Lilil","Lulut"}
 
-	// fmt.Printf("Semua Data =  %v\n",dataArray) //tampilan semua data yg ada di array
-	// fmt.Printf("data pertama = %v\n",dataArray[1]) //tampilan data array sesuai urutan
-	
-	// fmt.Printf("Masukan Nama User = ")
-	// fmt.Scan(&userNama)//inputan ke variabel biasa
+		fmt.Printf("Semua Data =  %v\n",dataArray) //tampilan semua data yg ada di array
+		fmt.Printf("data pertama = %v\n",dataArray[1]) //tampilan data array sesuai urutan
 
-	// fmt.Printf("Masukan Nama Array 2 = ")
-	// fmt.Scan(&dataArray[2])//inputan ke variabel array spesifik
+		fmt.Printf("Masukan Nama User = ")
+		fmt.Scan(&userNama)//inputan ke variabel biasa
 
-	// fmt.Printf("namanya %v\n",userNama)
-	// fmt.Printf("data terakhir = %v\n",dataArray[2])//ngambil array berdasarkan urutan
-	// fmt.Printf("data terakhir = %v\n",len(dataArray[2])) //ngambil panjang array
-	// fmt.Printf("data terakhir = %v\n",len(dataArray)) //ngambil jumlah data array ada berapa
-	// fmt.Printf("Semua Data =  %v\n",dataArray)
+		fmt.Printf("Masukan Nama Array 2 = ")
+		fmt.Scan(&dataArray[2])//inputan ke variabel array spesifik
 
+		fmt.Printf("namanya %v\n",userNama)
+		fmt.Printf("data terakhir = %v\n",dataArray[2])//ngambil array berdasarkan urutan
+		fmt.Printf("data terakhir = %v\n",len(dataArray[2])) //ngambil panjang array
+		fmt.Printf("data terakhir = %v\n",len(dataArray)) //ngambil jumlah data array ada berapa
+		fmt.Printf("Semua Data =  %v\n",dataArray)
+	*/
 	//array appen
-	var arrayAppend[]string
+	/*
+		var arrayAppend []string
+		var arrayAppend = []string{"Tony"}
+	*/
+	arrayAppend := []string{"Tony"}
 
 	fmt.Printf("Masukan Nama User = ")
-	fmt.Scan(&userNama)//inputan ke variabel biasa
+	fmt.Scan(&userNama) //inputan ke variabel biasa
 	arrayAppend = append(arrayAppend, userNama)
 
-	fmt.Printf("Semua Data =  %v\n",arrayAppend) //tampilan semua data yg ada di array
+	fmt.Printf("Semua Data =  %v\n", arrayAppend) //tampilan semua data yg ada di array
 }
